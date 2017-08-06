3b10f067c66aae52e5c59c608b259d768c8a4fb0
Erik Lindemann <lindemannerik@gmail.com>
2017-08-05 23:39:44 -0400

    Updated code to make the interface a little nicer. Also added an option for verbosity, you know, for nerds like me.

--- src/pigenerator/PiGenerator.java
+++ src/pigenerator/PiGenerator.java
@@ -13,22 +13,24 @@ public class PiGenerator {
     int bound;
     Random generator;
     int coPrimes;
+    boolean isVerbose;
     
-    public PiGenerator(int numberOfTrials, int bound) {
+    public PiGenerator(int numberOfTrials, int bound, boolean setVerbose) {
         this.numberOfTrials = numberOfTrials;
         this.bound = bound;
         this.generator = new Random();
+        this.isVerbose = setVerbose;
         this.coPrimes = 0;
     }
     
     public void Run() {
         for (int i = 0; i < numberOfTrials; i++) {
             BigInteger firstRandom = BigInteger.valueOf(generator.nextInt(bound));
-//            System.out.println("firstRandom: " + firstRandom);
+            if (isVerbose) System.out.println("firstRandom: " + firstRandom);
             BigInteger secondRandom = BigInteger.valueOf(generator.nextInt(bound));
-//            System.out.println("secondRandom: " + secondRandom);
+            if (isVerbose) System.out.println("secondRandom: " + secondRandom);
             BigInteger greatestCommon = firstRandom.gcd(secondRandom);
-//            System.out.println("GCD: " + greatestCommon);
+            if (isVerbose) System.out.println("GCD: " + greatestCommon);
             
             if (greatestCommon.intValue() == 1) {
                 coPrimes++;
@@ -37,18 +39,27 @@ public class PiGenerator {
         double probability = (double) coPrimes / numberOfTrials;
         double piEstimate = Math.sqrt(6.0/probability);
         System.out.println("Pi Estimate: " + piEstimate);
+        System.out.println("Happy \u03c0 Day!");
     }
     
     public static void main(String[] args) {
         
         Scanner reader = new Scanner(System.in);
-        System.out.println("Pi Estimator Tool!");
+        System.out.println("\u03c0 Estimator Tool!");
+        System.out.println("------------------------------------------------------");
+        System.out.println("The probability that two random numbers are coprime is 6/\u03c0\u00b2.");
+        System.out.println("So with a set of random numbers, and a little math, we can calculate \u03c0.");
+        System.out.println("------------------------------------------------------");
         System.out.print("How many trials: ");
         int trials = Integer.parseInt(reader.nextLine());
         System.out.print("Upper bound for randomization: " );
         int bound = Integer.parseInt(reader.nextLine());
+        System.out.print("Verbose y/n ? " );
+        boolean setVerbose = false;
+        char verbosity = reader.nextLine().toLowerCase().charAt(0);
+        if (verbosity == 'y') setVerbose = true;
         
-        PiGenerator generatomatic = new PiGenerator(trials, bound);
+        PiGenerator generatomatic = new PiGenerator(trials, bound, setVerbose);
         generatomatic.Run();       
         
     }
